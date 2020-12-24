3aa57b7dc817e8515eed7c38b62dae138b8f6cea
Fachri <fachrighiffary@gmail.com>
2020-12-24 20:48:35 +0700

    tinggal responsive

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,7 +6,8 @@ import Recipe3 from './image/recipe3.jpg'
 import Recipe4 from './image/recipe4.jpg'
 import Recipe5 from './image/recipe5.jpg'
 import Recipe6 from './image/recipe6.jpg'
-
+import Image1 from './image/img1.jpg'
+import Image2 from './image/img2.jpg'
 //icon
 import UserIcon from './icon/UserIcon.png'
 import LogoAuth from './icon/LogoAuth.svg';
@@ -16,4 +17,4 @@ import Edit from './icon/edit.png'
 
 
 
-export {ImgDashboard, UserIcon, LogoAuth, SearchIcon, Recipe1, Recipe2, Recipe3 ,Recipe4 ,Recipe5, Recipe6, Edit}
+export {ImgDashboard, UserIcon, LogoAuth, SearchIcon, Recipe1, Recipe2, Recipe3 ,Recipe4 ,Recipe5, Recipe6, Edit, Image1, Image2}
--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import { Edit } from '../../assets'
+import { Link, Route } from 'react-router-dom'
+import { Edit, Image1 } from '../../assets'
+import Navbar from '../../components/navbar'
 import './profile.css'
 
 export class Profile extends Component {
     render() {
         return (
             <>
+             <Route exact path={this.props.match.path} component={Navbar} />
             <div className="container section-profile d-flex justify-content-center flex-column align-items-center">
                 <div className="img-profile-section">
                     <img alt="" src="https://s3-alpha-sig.figma.com/img/d063/327b/4662c85cb7661f579e2c9baff0ce5fdc?Expires=1609718400&Signature=M5qACfjoeJDuvBOtvLGVoLgIA9LC-s~tm-2c3Cee9IhMMKGw0IuYa5BLg8zFyycBsSVSDw4hoFs8mHD9wfrmpIOpIjCfNs6uE9BWUNrhLL4lc9ojH60ba3mGu46whHNxCTb25mqaNjnXvEH6RdtCPWNZVQsKFPow~vc3p46WE1FPjRFr4zgXh41p6KKRe5lyBtVqkFgv5yxgahYyoGo1ZlAjO8MPNzj4aQLgrKZRJSHSLLkSrGhPJ5NIVo~UBo14w0ZOFcFFoYRq09S2hvxJjdUoCE9JotNJ7rHtXTWOfn1n8w9JTjgINDSvCPnWCbd3c0xG2GqsAk9eN5yjTeZ58Q__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" className="img-profile"/>
@@ -19,10 +21,33 @@ export class Profile extends Component {
                 </div>
             </div>
             <div className="container">
-                <div className="col-lg-5 d-flex justify-content-between bg-warning">
+                <div className="col-lg-5 d-flex justify-content-between">
                     <Link className="txt-profile text-decoration-none">My Recipe</Link>
-                    <Link className="txt-profile">Saved Recipe</Link>
-                    <Link className="txt-profile">Liked Recipe</Link>
+                    <Link className="txt-profile text-decoration-none">Saved Recipe</Link>
+                    <Link className="txt-profile text-decoration-none">Liked Recipe</Link>
+                </div>
+            </div>
+            <div className="row line"></div>
+            <div className="container d-flex flex-wrap  mt-4">
+                <div className="col-lg-3 cold-md-6 col-sm-6 d-flex justify-content-center mt-3">
+                    <div className="img-recipe-profile">
+                        <img className="img-recipe" alt="" src={Image1}/>
+                    </div>
+                </div>
+                <div className="col-lg-3 cold-md-6 col-sm-6 d-flex justify-content-center mt-3">
+                    <div className="img-recipe-profile">
+                        <img className="img-recipe" alt="" src={Image1}/>
+                    </div>
+                </div>
+                <div className="col-lg-3 cold-md-6 col-sm-6 d-flex justify-content-center mt-3">
+                    <div className="img-recipe-profile">
+                        <img className="img-recipe" alt="" src={Image1}/>
+                    </div>
+                </div>
+                <div className="col-lg-3 cold-md-6 col-sm-6 d-flex justify-content-center mt-3">
+                    <div className="img-recipe-profile">
+                        <img className="img-recipe" alt="" src={Image1}/>
+                    </div>
                 </div>
             </div>
             </>
