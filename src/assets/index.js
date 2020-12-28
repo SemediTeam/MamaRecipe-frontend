e708bfbafdb287236d91c02520ee874602eee11d
amardito <amardito37@gmail.com>
2020-12-29 05:27:12 +0700

    adding assets and package react player and material ui

--- src/assets/index.js
+++ src/assets/index.js
@@ -16,6 +16,8 @@ import UserIcon from './icon/defaultUserIcon.png'
 import LogoAuth from './icon/LogoAuth.svg';
 import SearchIcon from './icon/SearchIcon.svg'
 import Edit from './icon/edit.png'
+import PlayButton from './icon/trianglePlayButton.svg';
+import PauseButton from './icon/squarePauseButton.png';
 
 //loader
 import imgLoader from "./image/loader_main.gif";
@@ -23,5 +25,5 @@ import imgLoader from "./image/loader_main.gif";
 
 
 export {ImgDashboard, Recipe1, Recipe2, Recipe3 ,Recipe4 ,Recipe5, Recipe6, Popular, New, Image1, Image2}
-export {UserIcon, LogoAuth, SearchIcon, Edit}
+export {UserIcon, LogoAuth, SearchIcon, Edit, PlayButton, PauseButton}
 export {imgLoader}
