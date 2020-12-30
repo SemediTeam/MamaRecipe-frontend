44182b78b4bb4e856c44126755852cf0105f855d
amardito <amardito37@gmail.com>
2020-12-30 15:02:53 +0700

    delet unecessary

--- src/components/auth/resetPassword.jsx
+++ src/components/auth/resetPassword.jsx
@@ -51,7 +51,7 @@ class ResetPassword extends Component {
   }
 
   render() {
-    console.log(this.state.email, this.state.password)
+    // console.log(this.state.email, this.state.password)
     return (
       <>
         <Form className="w-100 mb-3 mt-3" onSubmit={this.handlerSubmit}>
